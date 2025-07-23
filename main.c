8950cf9c48bf46939bcf9d89af972711648ebf1b
BenjaminGonzales <benji.gonzales@gmail.com>
2025-07-23 19:44:57 -0400

    basic window with shifting colors; only handles quit event

--- main.c
+++ main.c
@@ -2,11 +2,74 @@
 // Created by Benji on 7/21/2025.
 //
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
-
-
-int main(void)
+int main(int argc, char* argv[])
 {
-    printf("test message!");
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "MyStuff",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        256, 240,
+        0
+        );
+
+    if (window == NULL)
+    {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 256, 240);
+    if (!texture) {
+        printf("SDL_CreateTexture failed: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Example pixel buffer (black screen)
+    uint32_t pixels[256 * 240] = {0}; // Initialize to black (RGBA = 0)
+
+    // Main loop
+    int running = 1;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        // Update texture with pixel data (replace with emulator frame buffer)
+        SDL_UpdateTexture(texture, NULL, pixels, 256 * sizeof(uint32_t));
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    // Cleanup
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
---
+++ sdl_test.c
@@ -0,0 +1,136 @@
+//
+// Created by Benji on 7/21/2025.
+//
+#include <SDL2/SDL.h>
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 320
+#define SDL_HINT_RENDER_SCALE_QUALITY 1
+
+typedef struct Emulator
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+} emulator_t;
+
+void vSdl_shutdown(emulator_t *emulator)
+{
+    SDL_DestroyRenderer(emulator->renderer);
+    SDL_DestroyWindow(emulator->window);
+    SDL_Quit();
+}
+
+int iSdl_init(emulator_t *p_emulator)
+{
+    int init_status = SDL_Init(SDL_INIT_EVERYTHING); // status 0 = success, < 0 = error
+    if (init_status < 0)
+    {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    p_emulator->window = SDL_CreateWindow(
+        "test_title_window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        0);
+    if (p_emulator->window == NULL)
+    {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    p_emulator->renderer = SDL_CreateRenderer(
+        p_emulator->window,
+        -1,
+        0);
+    if (p_emulator->renderer == NULL)
+    {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        return -1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    emulator_t emulator = {
+    .window = NULL,
+    .renderer = NULL
+    };
+
+    int init_status = iSdl_init(&emulator);
+    if (init_status != 0)
+    {
+        vSdl_shutdown(&emulator);
+        printf("initialization failure!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int x = 0, y = 0, r = 0, g = 0, b = 0;
+
+    enum shift_color
+    {
+        RED,
+        GREEN,
+        BLUE,
+    };
+    enum shift_color color = RED;
+
+    // main loop
+    int loop = 1;
+    while (loop)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                loop = 0;
+                break;
+            }
+        }
+        // SDL_SetRenderDrawColor(emulator.renderer, 0, 0, 0, 255);
+        // SDL_RenderClear(emulator.renderer);
+        SDL_SetRenderDrawColor(emulator.renderer, r, g, b, 255);
+        int test = SDL_RenderDrawLine(emulator.renderer, 0, y, SCREEN_WIDTH, y);
+        if (test)
+            printf("SDL_RenderDrawLine Error: %s\n", SDL_GetError());
+        SDL_RenderPresent(emulator.renderer);
+
+        x = (x + 1) % SCREEN_WIDTH;
+        y = (y + 1) % SCREEN_HEIGHT;
+
+        switch (color)
+        {
+            case RED:
+                r += 1;
+                if (r == 255)
+                {
+                    r = 0;
+                    color = GREEN;
+                }
+                break;
+            case GREEN:
+                g += 1;
+                if (g == 255)
+                {
+                    g = 0;
+                    color = BLUE;
+                }
+                break;
+            case BLUE:
+                b += 1;
+                if (b == 255)
+                {
+                    b = 0;
+                    color = RED;
+                }
+                break;
+        }
+        SDL_Delay(16);
+    }
+    vSdl_shutdown(&emulator);
+    exit(EXIT_SUCCESS);
+}
